d445cf6bf307549f597b35ba9926066cc97c9e82
Laszlo-Lazuer <visualjazz@gmail.com>
2018-03-15 22:56:55 -0500

    Adds header component & scss.

---
+++ src/Components/Header/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+  render () {
+  return  <div>
+    <h1>Header Component</h1>
+      <p>hello</p>
+    </div>
+  };
+}
+
+export default Header;
