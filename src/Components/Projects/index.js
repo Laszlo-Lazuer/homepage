cfce7274b930b685a12f4f9e02394ed758ad40bc
Laszlo-Lazuer <visualjazz@gmail.com>
2018-03-16 00:20:22 -0500

    Updates Projects page & index.

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 class CompProjects extends Component {
   render() {
-    return <h2>Component Projects</h2>;
+    return <div>
+      <ol>
+        <li><a href='https://laszlo-lazuer.github.io/React-Snake-Functional' target='_blank'>React Snake Functional Project</a></li>
+        <li><a href='https://laszlo-lazuer.github.io/road-to-react/' target='_blank'>React Hacker News Project</a></li>
+        <li><a href='https://laszlo-lazuer.github.io/react-redux-hackernews/' target='_blank'>Redux Hacker News Project</a></li>
+        <li><a href='https://laszlo-lazuer.github.io/spa-redux-todo/' target='_blank'>Redux To Do Project</a></li>
+      </ol>
+    </div>;
   }
 }
 
