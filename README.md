cc463aaf4331b7ca767012e79e4e5c6d11dba43e
Laszlo-Lazuer <visualjazz@gmail.com>
2018-07-14 19:05:10 -0500

    Updates repo.

--- README.md
+++
@@ -1,3 +0,0 @@
-Home Page Project in React + Router
-
-I will be using this repo to build out my Project Demo page.
--- src/Components/About/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class CompHome extends Component {
-  render() {
-    return <h2>Component About</h2>;
-  }
-}
-
-export default CompHome;
